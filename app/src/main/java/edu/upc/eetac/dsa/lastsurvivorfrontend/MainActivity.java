4aacf8c05c66e0c334ec20a5fd6d6a9aaa6227c0
Krunal <krunalmiracle@gmail.com>
2020-05-07 18:57:24 +0200

    Redesigned Login and Register Screen

--- app/src/main/java/edu/upc/eetac/dsa/lastsurvivorfrontend/MainActivity.java
+++ app/src/main/java/edu/upc/eetac/dsa/lastsurvivorfrontend/MainActivity.java
@@ -63,7 +63,40 @@ public class MainActivity extends AppCompatActivity {
         //TODO Else if user chose register than open register and transfer the connection
 
     }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
 
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    // Hide the nav bar and status bar
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Shows the system bars by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
     private void LaunchRegisterActivity() {
         Intent intent = new Intent(MainActivity.this ,RegisterActivity.class);
         /*
@@ -73,6 +106,10 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("DATA_4",0.6969);
         */
         startActivityForResult(intent,1);
+    }
+    @Override
+    public void onBackPressed(){
+        hideSystemUI();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
