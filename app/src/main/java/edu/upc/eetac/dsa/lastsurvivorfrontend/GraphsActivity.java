aa9ab5cd0d635ffeacd097016d29bb98d68c02b5
Waizzu <jerd91@gmail.com>
2020-06-23 15:18:28 +0200

    Graphics works! The names of the respective players are displayed. Players who have not played are not shown.

--- app/src/main/java/edu/upc/eetac/dsa/lastsurvivorfrontend/GraphsActivity.java
+++ app/src/main/java/edu/upc/eetac/dsa/lastsurvivorfrontend/GraphsActivity.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
@@ -31,12 +33,27 @@ import java.util.LinkedList;
 import java.util.List;
 
 import edu.upc.eetac.dsa.lastsurvivorfrontend.models.Player;
+import edu.upc.eetac.dsa.lastsurvivorfrontend.services.RankingService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class GraphsActivity extends AppCompatActivity {
 
     private ProgressBar pb_circular;
     List<Player> playerList = new LinkedList<>();
+
     Player player = new Player();
+    private static Retrofit retrofit;
+    private static String retrofitIpAddress;
+    RankingService rankingService;
+    public TextView username;
+
+    float x1, x2, y1, y2;
 
 
     @Override
@@ -44,195 +61,211 @@ public class GraphsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-        //playerList = getIntent().getParcelableArrayListExtra("playerList");//recuperamos los jugadores
         pb_circular = findViewById(R.id.progressBar_cyclic);
-        //player = getIntent().getParcelableExtra("Player");
-        //playerList = getIntent().getParcelableArrayListExtra("playerList");
-        // Bundle bundle = getIntent().getExtras();
-        // playerList = bundle.getParcelable("data");
-        int size= playerList.size();
+
         ListView lv = (ListView) findViewById(R.id.listView1);
-        Log.d("Cantidad de jugadores: ", String.valueOf(playerList.size()));
 
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        //Attaching Interceptor to a client
+        OkHttpClient client = new OkHttpClient().newBuilder().addInterceptor(interceptor).build();
+        ResourceFileReader rs = new ResourceFileReader();
+        retrofitIpAddress = ResourceFileReader.ReadResourceFileFromStringNameKey("retrofit.IpAddress", this);
+        startRetrofit();
+        rankingService = retrofit.create(RankingService.class);
+        getRank(lv);
 
-        final String[] Nombres = new String[] {"Midoriya", "kru", "juan", "paco"};
-        ArrayList<BarData> list = new ArrayList<>();
-
-        for (int i = 0; i<6; i++) {//aqui deberia de haber la cantidad de graficos que queremos poner,teniendo un intento no fallido extraer playerlist.size();
-            list.add(generateData(i + 1));
-
-        }
-        ChartDataAdapter chartDataAdapter = new ChartDataAdapter(getApplicationContext(), list);
-        lv.setAdapter(chartDataAdapter);
         pb_circular.setVisibility(View.GONE);
 
     }
 
     class ChartDataAdapter extends ArrayAdapter<BarData> {
-        public ChartDataAdapter(Context context, List<BarData> objects) {
+        public ChartDataAdapter(Context context, List<BarData> objects, List<Player> playerList) {
             super(context, 0, objects);
         }
+
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-            Log.d("Cantidad de jugadores: ", String.valueOf(playerList.size()));
-
 
             BarData data = getItem(position);
+
             ViewHolder holder = null;
 
             if (convertView == null) {
                 holder = new ViewHolder();
                 convertView = getLayoutInflater().from(getContext()).inflate(R.layout.activity_graph_aux, null);
+                TextView username = findViewById(R.id.playerNameChart);
                 holder.chart = (BarChart) convertView.findViewById(R.id.chart);
+                holder.username = (TextView) convertView.findViewById(R.id.playerNameChart);
                 convertView.setTag(holder);
             } else {
                 holder = (ViewHolder) convertView.getTag();
-
             }
 
             data.setValueTextColor(Color.BLACK);
+            data.setValueTextSize(15);//el tamaño de los valores de las barras
             //PARAMETROS DE PINTAR LOS GRAFICOS
             holder.chart.getDescription().setEnabled(false);
             holder.chart.setScaleEnabled(false);//desactivar el zoom de barras
             holder.chart.setDrawGridBackground(false);
             holder.chart.setBackgroundColor(Color.WHITE);
-            //holder.chart.setDrawBarShadow(true);
+            holder.chart.setDrawValueAboveBar(true);
+            holder.chart.setFitBars(true);
+            holder.chart.getDescription().setEnabled(false);
+            holder.chart.getDescription().setTextSize(20);
+
+
+            holder.chart.getDescription().setText(playerList.get(position).getUsername());
+            holder.username.setText(playerList.get(position).getUsername());
+
+            // holder.chart.getDescription().setPosition();
+
             ViewHolder finalHolder = holder;
-            holder.chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
-                @Override
-                public void onValueSelected(Entry e, Highlight h) {
-                    finalHolder.chart.getXAxis().getValueFormatter().getFormattedValue(e.getX(), finalHolder.chart.getXAxis());
-                }
 
-                @Override
-                public void onNothingSelected() {
 
-                }
-            });
+            final ArrayList<String> xVals = new ArrayList<>();
+            final String[] labels = {"experience", "kills", "games", "monsters per game"};
 
-            final String[] labels = new String[] {"Experience", "Kills", "GamesPlayed", "Credits"};
+            Legend l = holder.chart.getLegend();
+            l.setFormSize(10f); // set the size of the legend forms/shapes
+            l.setForm(Legend.LegendForm.DEFAULT); // set what type of form/shape should be used
+            // l.setPosition(LegendPosition.BELOW_CHART_LEFT);
+            //l.setTypeface(...);
+            l.setTextSize(12f);
+            l.setTextColor(Color.BLACK);
+            l.setXEntrySpace(5f); // set the space between the legend entries on the x-axis
+            l.getCalculatedLabelSizes();
+
+            // set custom labels and colors
+
+            data.setValueTextColor(Color.BLACK);
+
             XAxis xAxis = holder.chart.getXAxis();
-            xAxis.setValueFormatter(new MyXAxisValueFormatter(labels));
-            xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
             xAxis.setDrawGridLines(false);
             xAxis.setGranularity(1);
-            xAxis.setTextColor(Color.WHITE);
-            xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
+
+            xAxis.setLabelCount(4);
+            xAxis.setDrawLabels(false);
+            xAxis.setTextSize(20);
             // xAxis.setAxisMaximum(1);
-            // xAxis.setCenterAxisLabels(true);
+            xAxis.setCenterAxisLabels(false);
 
             YAxis leftAxis = holder.chart.getAxisLeft();
             leftAxis = holder.chart.getAxisLeft();
+            holder.chart.getAxisLeft().setDrawLabels(false);
+            holder.chart.getAxisLeft().setDrawAxisLine(true);
+            holder.chart.getAxisLeft().setDrawGridLines(false);
             leftAxis.setLabelCount(5, false);
-            leftAxis.setTextColor(Color.WHITE);
+            leftAxis.setTextColor(Color.BLACK);
+            leftAxis.setAxisMinimum(0);//colocar donde empieza el grafico
             leftAxis.setSpaceTop(15f);
+            holder.chart.getAxisRight().setDrawAxisLine(true);
+            holder.chart.getAxisRight().setDrawLabels(true);
             YAxis rightAxis = holder.chart.getAxisRight();
-            rightAxis.setLabelCount(5, false);
+            rightAxis.setLabelCount(5, true);
+            rightAxis.setAxisMinimum(0);
             rightAxis.setSpaceTop(15f);
-
+            rightAxis.setSpaceBottom(0);
+            holder.chart.setExtraTopOffset(60);
             holder.chart.setData(data);
             holder.chart.setFitBars(true);
-            holder.chart.animateY(750);
-            //holder.chart.animateXY(500, 500);//este valdria para animar en ambos sentidos
+            holder.chart.animateY(500);
             pb_circular.setVisibility(View.GONE);
 
             return convertView;
 
-
-            //return super.getView(position,convertView,parent);
-
         }
+
         class ViewHolder {
+            public TextView username;
             BarChart chart;
         }
     }
-    private BarData generateData(int count) {
+
+    private BarData generateData(int count, List<Player> playerList) {//pasar posicion de un jugador y la lista jugadores
         ArrayList<BarEntry> entries = new ArrayList<>();
 
-        for (int i = 0; i < 5; i++) {//las barras serian os campos recibidos de cada objeto player
-            entries.add(new BarEntry(i, (float) (Math.random() * 90) + 30)); //valor de las barras exp,kills, etc
 
-        }
-        // string del user 0 de la lista
-        BarDataSet dataSet = new BarDataSet(entries, "Jugador: " + count);
-        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        entries.add(new BarEntry(0, (int) playerList.get(count).getExperience()));
+        entries.add(new BarEntry(1, (float) playerList.get(count).getKills()));
+        entries.add(new BarEntry(2, (float) playerList.get(count).getGamesPlayed()));
+        entries.add(new BarEntry(3, (float) (playerList.get(count).getKills()) / (playerList.get(count).getGamesPlayed())));
+        // username.setText(playerList.get(count).getUsername());
+        BarDataSet dataSet = new BarDataSet(entries, "");
+
+
+        dataSet.setLabel("Experience · Kills · Games · Kills x Game  ");
+
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        //dataSet.setValueTextSize(40f); si se introduce aqui luego causa errores al volver a entrar al al GraphsActivity desde el rankingactivity creando numeros monstruosamente grandes
         dataSet.setBarShadowColor(Color.rgb(203, 203, 203));
         BarData data = new BarData(dataSet);
         data.setBarWidth(0.9f);
+
+
         return data;
     }
 
+    private static void startRetrofit() {
+        //HTTP &
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        //Attaching Interceptor to a client
+        OkHttpClient client = new OkHttpClient().newBuilder().addInterceptor(interceptor).build();
 
-    public class MyXAxisValueFormatter extends IndexAxisValueFormatter {
-        private String[] mValues;
-        public MyXAxisValueFormatter(String[] values){
-            this.mValues = values;
-        }
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://" + retrofitIpAddress + ":8080/Backend/")
+                .addConverterFactory(new NullOnEmptyConverterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+    }
 
-        @Override
-        public String getFormattedValue(float value, AxisBase axis){
-            return mValues[(int)value];
+    private void getRank(ListView lv) {
+
+        pb_circular.setVisibility(View.VISIBLE);
+        try {
+
+            Call<List<Player>> playersStats = rankingService.rankingList();
+            /* Android Doesn't allow synchronous execution of Http Request and so we must put it in queue*/
+            playersStats.enqueue(new Callback<List<Player>>() {
+                @Override
+                public void onResponse(Call<List<Player>> call, Response<List<Player>> response) {
+
+                    pb_circular.setVisibility(View.GONE);
+                    //Retrieve the result containing in the body
+                    if (!response.body().isEmpty()) {
+                        // non empty response, Mapping Json via Gson...
+                        Log.d("GraphsActivity", "Server Response Ok");
+                        playerList = response.body();
+
+                        ArrayList<BarData> list = new ArrayList<>();
+                        for (int i = 0; i < playerList.size(); i++) //aqui deberia de haber la cantidad de graficos "items" que queremos poner insertar
+                        {
+                            if (playerList.get(i).getExperience() > 0) {
+                                list.add(generateData(i, playerList));
+                            }
+                        }
+                        ChartDataAdapter chartDataAdapter = new ChartDataAdapter(getApplicationContext(), list, playerList);//pasar como parametro tambien playerlist?
+                        lv.setAdapter(chartDataAdapter);
+
+                    } else {
+                        // empty response...
+                        Log.d("GraphsActivity", "Request Failed!");
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<Player>> call, Throwable t) {
+                    //  NotifyUser("Error,could not retrieve data!");
+                }
+            });
+        } catch (Exception e) {
+            Log.d("GraphsActivity", "Exception: " + e.toString());
         }
 
     }
 }
-        /*for (Player player : playerList)
-            Log.d("value is" , playerList.toString());*/
-//ArrayList
-        /*mpLinechart=(LineChart) findViewById(R.id.line_chart);
-        LineDataSet lineDataSet1 = new LineDataSet(dataValues1(),"Data Set 1");//Datos1
-        LineDataSet lineDataSet2 = new LineDataSet(dataValues1(),"Data Set 2");//Datos2
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(lineDataSet1);
-        dataSets.add(lineDataSet2);
-
-        TextView text = findViewById(R.id.textView9);
-        Intent intent = getIntent();
-
-        //ArrayList<String> mylist= getIntent().getStringArrayListExtra("mylist");
-
-        //text.setText((CharSequence) mylist);
-
-
-        //caracteristicas del grafico
-        mpLinechart.setNoDataText("NO DATA");
-        mpLinechart.setNoDataTextColor(Color.RED);
-        mpLinechart.setBackgroundColor(Color.WHITE);
-        mpLinechart.setDrawGridBackground(true);
-        mpLinechart.setDrawBorders(true);
-        mpLinechart.setBorderColor(Color.RED);
-
-        LineData data = new LineData(dataSets);
-        mpLinechart.setData(data);// pone los datos en el grafico, si no hay salta el no data
-        mpLinechart.invalidate();
-        pb_circular.setVisibility(View.GONE);*/
-
-//datos para añadir, aqui se deberia de poner datos recibidos de BBDD
-   /* private ArrayList<Entry> dataValues1()
-    {
-        ArrayList<Entry>dataVals= new ArrayList<Entry>();
-        dataVals.add((Entry) new Entry(0,80));
-        dataVals.add((Entry) new Entry(1,0));
-        dataVals.add((Entry) new Entry(2,35));
-        dataVals.add((Entry) new Entry(3,5));
-        dataVals.add((Entry) new Entry(4,70));
-        return dataVals;
-    }
-    private ArrayList<Entry> dataValues2()
-    {
-        ArrayList<Entry>dataVals= new ArrayList<Entry>();
-        dataVals.add((Entry) new Entry(0,20));
-        dataVals.add((Entry) new Entry(1,30));
-        dataVals.add((Entry) new Entry(2,20));
-        dataVals.add((Entry) new Entry(3,60));
-        dataVals.add((Entry) new Entry(4,20));
-        return dataVals;
-    }*/
-   /* private String getPlayerStats(){
-        int baseExp = 50;
-        int level = player.getExperience() /baseExp;
-        int exp = player.getExperience()%baseExp;
-        return "P,"+level+","+exp+","+player.getKills()+","+player.getCredits();
-    }*/
